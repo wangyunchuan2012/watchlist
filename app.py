bfedc89115fae769217dbc9cc09a70b7df6d83d0
wangyunchuan2012 <237563870@qq.com>
2022-07-10 11:55:11 +0800

    add index page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/home')
-def home_page():
+def home_page():  # 视图函数
     return 'hello, this is home page!'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)  # 调用模板
 
 
 @app.route('/user/<name>')
@@ -27,3 +32,16 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
 
     return 'Test page'
+
+
+name = 'Grey Li'
+
+movies = [
+
+    {'title': 'My Neighbor Totoro', 'year': '1988'}, {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'}, {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'}, {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'}, {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'}, {'title': 'The Pork of Music', 'year': '2012'},
+
+]

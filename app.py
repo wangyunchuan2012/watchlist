d9b82678af7bbd5128e9b4866a52dadac7cf2746
wangyunchuan2012 <237563870@qq.com>
2022-07-11 10:54:03 +0800

    add index page

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask import url_for
+import os
+import sys
+import click
+
+WIN = sys.platform.startswith('win')
+
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
 
 @app.route('/home')
@@ -34,6 +48,16 @@ def test_url_for():
     return 'Test page'
 
 
+@app.cli.command()  # 注册命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置默认参数
+def initdb(drop):
+    'initialize the database'
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
 name = 'Grey Li'
 
 movies = [
@@ -45,3 +69,14 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'}, {'title': 'The Pork of Music', 'year': '2012'},
 
 ]
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
